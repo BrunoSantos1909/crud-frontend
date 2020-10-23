400a0062c039a4e390a381ede53141b9a8b3a341
Bruno Santos <bruno@econodata.com.br>
2020-10-23 11:27:44 -0300

    first commit

---
+++ src/components/Aluno.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <b-table :items="items" show-empty>
+        
+
+        </b-table>
+        aaaaaaaaaa
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            items: [],
+
+        }
+    }
+}
+</script>
